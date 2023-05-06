9fb933324ed30f088f9b6a6d9801a14de8df2324
Daniele Lanzetta <lanzetta3@gmail.com>
2023-05-06 19:35:39 +0200

    test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,8 @@ export default function Home() {
         <title>Octo.</title>
         <meta
           name="description"
-          content="By leveraging insights from our network of industry insiders, you’ll know exactly when to buy to maximize profit, and exactly when to sell to avoid painful losses."
+          content="By leveraging insights from our network of 
+          industry insiders, you’ll know exactly when to buy to maximize profit, and exactly when to sell to avoid painful losses."
         />
       </Head>
       <Header />
